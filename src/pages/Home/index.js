950e2951d474cbe164965e2dda4928bf7e066202
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-12-29 21:43:25 +0530

    header section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import FAQ from '../../common/Components/faq';
+import HeaderSection from './Components/HeaderSection';
 
 function Home() {
   return (
     <div>
+      <HeaderSection />
       <FAQ />
     </div>
   );
